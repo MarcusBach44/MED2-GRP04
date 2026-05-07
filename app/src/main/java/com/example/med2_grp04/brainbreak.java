bb061786effbd5d3efc1072c9849f37978a2e7f5
magnusgrabow <magnusgrabow@gmail.com>
2026-05-07 14:34:16 +0200

    Hjerne pauser

--- app/src/main/java/com/example/med2_grp04/brainbreak.java
+++ app/src/main/java/com/example/med2_grp04/brainbreak.java
@@ -62,10 +62,15 @@ public class brainbreak extends AppCompatActivity {
     private void toggleBrainBreak(String name, ImageButton button) {
         if (!activeSettings.contains(name)) {
             activeSettings.add(name);
-            button.setBackgroundTintList(android.content.res.ColorStateList.valueOf(getResources().getColor(R.color.black)));
+            button.setBackgroundTintList(android.content.res.ColorStateList.valueOf(getResources().getColor(R.color.lightgrey)));
+
+            button.setImageTintList(null);
+            button.clearColorFilter();
         } else {
             activeSettings.remove(name);
             button.setBackgroundTintList(android.content.res.ColorStateList.valueOf(getResources().getColor(R.color.grey)));
+
+            button.setImageTintList(android.content.res.ColorStateList.valueOf(android.graphics.Color.argb(150, 100, 100, 100)));
         }
         saveSettings();
     }
@@ -78,14 +83,21 @@ public class brainbreak extends AppCompatActivity {
         editor.apply();
     }
     private void loadSettings() {
+        setButtonState(btnTicTacToe, false);
+        setButtonState(btnMineSweeper, false);
+        setButtonState(btnWordle, false);
+        setButtonState(btnSudoku, false);
+
         android.content.SharedPreferences sharedPreferences = getSharedPreferences("BrainBreakPrefs", MODE_PRIVATE);
         String settingsString = sharedPreferences.getString("active_games", "");
 
         if (!settingsString.isEmpty()) {
             String[] games = settingsString.split(",");
             for (String game : games) {
-                activeSettings.add(game);
-                updateButtonUI(game);
+                if (!activeSettings.contains(game)) {
+                    activeSettings.add(game);
+                    updateButtonUI(game);
+                }
             }
         }
     }
@@ -98,7 +110,20 @@ public class brainbreak extends AppCompatActivity {
         else if (name.equals("Sudoku")) btn = btnSudoku;
 
         if (btn != null) {
-            btn.setBackgroundTintList(android.content.res.ColorStateList.valueOf(getResources().getColor(R.color.black)));
+            btn.setBackgroundTintList(android.content.res.ColorStateList.valueOf(getResources().getColor(R.color.lightgrey)));
+            btn.setImageTintList(null);
+            btn.clearColorFilter();
+        }
+    }
+    private void setButtonState(ImageButton btn, boolean active) {
+        if (btn == null) return;
+        if (active) {
+            btn.setBackgroundTintList(android.content.res.ColorStateList.valueOf(getResources().getColor(R.color.lightgrey)));
+            btn.setImageTintList(null);
+            btn.clearColorFilter();
+        } else {
+            btn.setBackgroundTintList(android.content.res.ColorStateList.valueOf(getResources().getColor(R.color.grey)));
+            btn.setImageTintList(android.content.res.ColorStateList.valueOf(android.graphics.Color.argb(150, 100, 100, 100)));
         }
     }
 }
