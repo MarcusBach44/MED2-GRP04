420b6d70a05d2c20e7e8d909ca0b5540c1092bdd
Patrick47848448 <patr8997@edu.mercantec.dk>
2026-05-11 08:16:57 +0200

    Update InstigateGames.java

--- app/src/main/java/com/example/med2_grp04/InstigateGames.java
+++ app/src/main/java/com/example/med2_grp04/InstigateGames.java
@@ -10,6 +10,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
+import java.util.ArrayList;
+import java.util.Random;
 
 public class InstigateGames {
 
